f62ebb46ba96954f488cc8e5845aac846eb354e6
Tuntunyawat <twinyu0245@gmail.com>
2023-06-07 19:14:25 +0200

    UPADTATE ES10 JS

---
+++ esercizi-di-JAVASCRIPT/esercizio10/es10.js
@@ -0,0 +1,21 @@
+
+const promise = new Promise((resolve, reject) => {
+
+    const promise = false;
+
+    if(promise == true){
+        resolve(promise);
+
+    }else{
+        reject(new Error(`Promessa non tenuta`));
+    }
+});
+
+
+promise
+    .then(result => {
+        console.log(`Operazione e andato a buon fine:` , result);
+    })
+    .catch(error => {
+        console.log(`Si e verificato un errore`, error);
+    })
